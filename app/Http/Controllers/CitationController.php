327fe8bdcccd18ff064a212c4cd6240af8c469d7
Valentyna Chuvashova <zirochka1983@gmail.com>
2019-07-09 14:55:59 +0300

    last commit

--- app/Http/Controllers/CitationController.php
+++ app/Http/Controllers/CitationController.php
@@ -6,16 +6,19 @@ use Illuminate\Http\Request;
 
 class CitationController extends Controller
 {
+    protected $citation = [
+        'Если ты не знаешь, чего хочешь, ты в итоге останешься с тем, чего точно не хочешь. ',
+        'Чтобы дойти до цели, надо идти. ',
+        'Это своего рода забава, делать невозможное. ',
+        'Если люди не смеются над вашими целями, значит ваши цели слишком мелкие. ',
+        'Пробуйте и терпите неудачу, но не прерывайте ваших стараний.',
+        'Запомните - этот день возврату и обмену не подлежит. '
+    ];
+
     public function show()
     {
-       // $citation = 'bla bla bla';
-        $citation[] = 'Если ты не знаешь, чего хочешь, ты в итоге останешься с тем, чего точно не хочешь. ';
-        $citation[] = 'Чтобы дойти до цели, надо идти. ';
-        $citation[] = ' Это своего рода забава, делать невозможное. ';
-        $citation[] = ' Если люди не смеются над вашими целями, значит ваши цели слишком мелкие. ';
-        $citation[] = 'Пробуйте и терпите неудачу, но не прерывайте ваших стараний.';
-        srand ((double) microtime() * 1000000);
-        $random_number = rand(0,count($citation)-1);
+        $citation = $this->citation;
+        $random_number = rand(0, count($citation) - 1);
         $citation = $citation[$random_number];
         return view('inspire', compact('citation'));
     }
