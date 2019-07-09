1eb135426299ceeee568882de9858f1c0a4144c9
Valentyna Chuvashova <zirochka1983@gmail.com>
2019-07-09 14:13:59 +0300

    second task phrases

--- app/Http/Controllers/CitationController.php
+++ app/Http/Controllers/CitationController.php
@@ -8,8 +8,15 @@ class CitationController extends Controller
 {
     public function show()
     {
-        $citation = 'bla bla bla';
-
+       // $citation = 'bla bla bla';
+        $citation[] = 'Если ты не знаешь, чего хочешь, ты в итоге останешься с тем, чего точно не хочешь. ';
+        $citation[] = 'Чтобы дойти до цели, надо идти. ';
+        $citation[] = ' Это своего рода забава, делать невозможное. ';
+        $citation[] = ' Если люди не смеются над вашими целями, значит ваши цели слишком мелкие. ';
+        $citation[] = 'Пробуйте и терпите неудачу, но не прерывайте ваших стараний.';
+        srand ((double) microtime() * 1000000);
+        $random_number = rand(0,count($citation)-1);
+        $citation = $citation[$random_number];
         return view('inspire', compact('citation'));
     }
 }
--- resources/views/inspire.blade.php
+++ resources/views/inspire.blade.php
@@ -62,7 +62,7 @@
 <div class="flex-center position-ref full-height">
     <div class="content">
         <div class="title m-b-md">
-            {{ $citation }}
+            {{ $citation}}
         </div>
     </div>
 </div>
